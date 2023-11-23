0de30de47a73a41d4c53470f4e581e2cf357a134
Tris Oaten <tris@oat.sh>
2023-11-23 16:30:20 +0000

    move to rstml

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,56 @@
 use color_eyre::Report;
-use html_to_string_macro::html;
+use html_node::{
+    typed::{elements::*, html},
+    Node,
+};
+use rstml::node::RawText;
 
-fn template(inner: String) -> String {
-    let page_style = r#"
-      body {
+fn template(inner: Node) -> Node {
+    let page_style_tag = html! {
+        <style>
+          body {
+            text-align: center;
+            font-family: courier, monospace;
+            font-size: 2rem;
+            margin: 20px;
+            background-color: #000000;
+            padding: 10px;
+            color: #fff;
+          }
+          .justify {
+            text-align: justify;
+            text-justify: inter-word;
+          }
+          a, .white, h1, h2 {
+            color: white;
+          }
+          .slogan {
+            font-family: serif;
+            font-size: x-large;
+            font-weight: bold;
+          }
+          .narrow {
+             max-width: 50%;
+          }
+          h1 {
+            max-width: 40rem;
+            line-height: 3rem;
+          }
+          .titles {
+            font-size: 3rem;
+            font-weight: bold;
+          }
+        img{
+            width:100%;
+            max-width:300px;
+        }
+        </style>
+    };
 
-        font-family: courier, monospace;
-        font-size: 2rem;
-        margin: 20px;
-        background-color: #000000;
-        padding: 10px;
-        color: #fff;
-        //background-image: url('stars.jpeg');
-        //background-repeat: no-repeat;
-        //background-attachment: fixed;
-        //background-size: cover;
-      }
-      .justify {
-        text-align: justify;
-        text-justify: inter-word;
-      }
-      a, .white, h1, h2 {
-        color: white;
-      }
-      .slogan {
-        font-family: serif;
-        font-size: x-large;
-        font-weight: bold;
-      }
-      }
-      .narrow {
-         max-width: 50%;
-      }
-      h1 {
-        max-width: 40rem;
-        line-height: 3rem;
-      }
-      .titles {
-        font-size: 3rem;
-        font-weight: bold;
-      }
-    img{
-        width:100%;
-        max-width:300px;
-    }
-"#;
     html! {
-           <!DOCTYPE html>
-           <html lang="en">
-
-           <head>
+        <!DOCTYPE html>
+        <html lang="en">
+            <head>
                <meta http-equiv="x-clacks-overhead" content="GNU Terry Pratchett" />
                <link rel="icon" href="favicon.png"/>
 
@@ -59,42 +60,48 @@ fn template(inner: String) -> String {
                <meta content="width=device-width, initial-scale=1" name="viewport"/>
                <title>"The Phosphene Catalogue Podcast"</title>
 
-               <style>
+               { page_style_tag }
 
-               {page_style}
+            </head>
 
-               </style>
+            <body>
 
-           </head>
+                <div class="titles">
+                    <a href="">Listen</a>
+                    <div></div>
+                    <a href="">Mastodon</a>
+                    <div></div>
+                    <a href="https://discord.gg/mCY2bBmDKZ">Discord</a>
+                    <div></div>
+                    <a href="">Patreon</a>
+                </div>
 
-    <body>
+                <br/>
+                <br/>
 
-    <center>
-    <div class="titles">
-        <a href="">"Listen"</a>"&nbsp; "
-        <a href="">"Mastodon"</a>"&nbsp; "
-        <a href="https://discord.gg/mCY2bBmDKZ">"Discord"</a>"&nbsp; "
-        <a href="">"Patreon"</a>"&nbsp; "
-    </div>
-    <br/>
-    <br/>
+                <a href="index.html">
+                    <img alt="A photo of an art catelogue cover" src="logo.jpg" width="50%"/>
+                </a>
 
+               {inner}
 
-    <a href="index.html">
-        <img alt="A photo of an art catelogue cover" src="logo.jpg" width="50%"/>
-    </a>
-
-       {inner}
-
-    </center>
-    </body>
-    </html>
+            </body>
+        </html>
     }
 }
 
-fn index() -> String {
+fn index() -> Node {
+    // NOTE: the widget requires https to load
+    let soundcloud_widget = html_node::html! {
+        <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1657202067&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=false">
+            </iframe>
+            <div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;">
+            <a href="https://soundcloud.com/namtao" title="namtao" target="_blank" style="color: #cccccc; text-decoration: none;">namtao</a>
+            <a href="https://soundcloud.com/namtao/demo-the-phosphene-catalogue-theme" title="[DEMO] The Phosphene Catalogue Theme" target="_blank" style="color: #cccccc; text-decoration: none;">[DEMO] The Phosphene Catalogue Theme</a>
+            </div>
+    };
+
     template(html! {
-    <center>
           <div class="slogan"> "We see light where others see only darkness." </div>
           <br/>
           <br/>
@@ -102,7 +109,7 @@ fn index() -> String {
           "An urban fantasy podcast of tape recordings by the curator of a secrative London-based art auction house."
           <br/>
           <br/>
-          "<b>The Phosphene Catalogue</b> is a mail-order catalogue from the 1970s, specialising in those items that cannot be sold at other auction houses: Paintings of lost origin, statues that are 'too grotesque' for public display, and books better left unread."
+          <b>The Phosphene Catalogue</b> is a 1970s mail-order catalogue, specialising in those items that cannot be sold at other auction houses: Paintings of lost origin, statues that are "too grotesque" for public display, and books better left unread...
           </div>
       <div class="narrow">
       </div>
@@ -110,15 +117,17 @@ fn index() -> String {
     <br/>
 
         <b><a href="">"Listen to the pilot here"</a></b>
+        { soundcloud_widget }
         <br/>
         <br/>
-    </center>
     })
 }
 
-fn build(pages: Vec<(&str, fn() -> String)>) -> Result<(), Report> {
+type Router<'a> = Vec<(&'a str, fn() -> Node)>;
+
+fn build(pages: Router) -> Result<(), Report> {
     for (page, fun) in pages {
-        std::fs::write(page, fun())?;
+        std::fs::write(page, fun().to_string())?;
     }
     Ok(())
 }
