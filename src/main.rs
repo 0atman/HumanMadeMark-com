cf7d292febaffe7d47f275d15c1e43ce4b6ccc20
Tris Oaten <tris@oat.sh>
2024-07-03 14:20:50 +0100

    Humanmade website init

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn template(inner: Node) -> Node {
                 <meta http-equiv="x-clacks-overhead" content="GNU Terry Pratchett" />
                 <link rel="icon" href="favicon.png"/>
                 <script src="https://cdn.tailwindcss.com"></script>
+                <script src="tw.js"></script>
 
             <script>
                 tailwind.config = {
@@ -32,7 +33,7 @@ fn template(inner: Node) -> Node {
             <meta name="description" content="An urban fantasy podcast of tape recordings by the curator of a secretive london-based art auction house."/>
 
             <meta content="width=device-width, initial-scale=1" name="viewport"/>
-            <title>"The Phosphene Catalogue Podcast"</title>
+            <title>"The Humanmade Mark"</title>
 
             </head>
 
@@ -40,34 +41,24 @@ fn template(inner: Node) -> Node {
 
                     <nav class="flex items-center justify-between flex-wrap bg-black-500 p-6">
                         <div class="flex items-center flex-shrink-0 text-white mr-6">
-                            <span class="font-semibold text-xl tracking-tight">The Phosphene Catalogue</span>
+                            <span class="font-semibold text-xl tracking-tight">The Humanmade Mark</span>
                         </div>
                         <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
                             <div class="text-xl lg:flex-grow">
-                                <a href="https://www.spreaker.com/show/the-phosphene-catalogue" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Listen
+                                <a href="index.html#about" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+                                    About
                                 </a>
-                                <a href="https://open.spotify.com/show/5XPmpYIlK2nYOfINCzlYUu?si=00798415bfff4b9e" class="blocklg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Spotify
+                                <a href="black-logo.png" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+                                    Black.png
                                 </a>
-                                <a href="https://www.spreaker.com/show/6029902/episodes/feed" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    RSS
+                                <a href="white-logo.png" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+                                    White.png
                                 </a>
-                                <a href="https://masto.namtao.com/@PhospheneCatalogue" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Mastodon
+                                <a href="black-logo.svg" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+                                   Black.svg
                                 </a>
-                                <a href="https://discord.gg/mCY2bBmDKZ" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Discord
-                                </a>
-                                <a href="https://www.teepublic.com/user/the-phosphene-catalogue" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Store
-                                </a>
-                                <a href="https://soundcloud.com/namtao/sets/the-phosphene-catalogue-music" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Music
-                                </a>
-
-                                <a href="https://www.patreon.com/PhospheneCatalogue" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
-                                    Patreon
+                                <a href="white-logo.svg" class="block lg:inline-block lg:mt-0 text-black-200 hover:text-white mr-4">
+                                    White.svg
                                 </a>
                             </div>
                         </div>
@@ -76,7 +67,7 @@ fn template(inner: Node) -> Node {
                     <div class="border-black border-8 container mx-auto">
 
                         <div class="flex w-full justify-center">
-                            <img class="w-1/2" alt="A photo of an art catelogue cover" src="logo.png" />
+                            <img class="w-1/2" alt="A logo of a human with a seed germinating in their head, with the words 'Humanmade' next to it, along with the website humanmademark.com underneath." src="white-logo.svg" />
                         </div>
 
                             <br/>
@@ -99,27 +90,90 @@ fn widget() -> Node {
 
 fn index() -> Node {
     template(html! {
-          <div class="slogan"> "We see light where others see only darkness." </div>
+              <div class="slogan"><b> "When you see this logo on any artwork, whether painting, poetry, or prose, you know that it was made by a human just like you." </b></div>
 
-          <br/>
-          <br/>
-        <div class="justify">
-          "An urban fantasy podcast of tape recordings by the curator of a secretive London-based art auction house."
-          <br/>
-          <br/>
-          The Phosphene Catalogue is a 1970s mail-order catalogue, specialising in those items that cannot be sold at other auction houses: Paintings of lost origin, statues that are "too grotesque" for public display, and books better left unread...
+              <br/>
+              <br/>
+            <div class="justify">
+
+
+    "Hi friends, I'd like to introduce you to a small project I've made."
+    <br/>
+    <br/>
+    "The "<i>Humanmade</i>" mark, here, is something I will be attaching to any works of mine that were mostly made by me or my friends, not by generative tools like GPT. I've built this website to freely share the high-resolution black or white versions of the logo available with you, which you can download and attach to your own projects if you'd like to make the same statement."
+
+    <br/>
+    "I've made the following video to try to make my reasons for making this clear, but it's simple:"
+
+    <br/>
+    <br/>
+    <b>"I don't hate AIs,"</b>
+    <br/>
+    <b>"I love humans!"</b>
+        </div>
+          <div class="narrow">
           </div>
-      <div class="narrow">
-      </div>
     <br/>
     <br/>
 
-        <b><a href="https://www.youtube.com/watch?v=y3wVvDbJHrU">"Listen to the first issue here"</a></b>
+            <b><a href="https://www.youtube.com/watch?v=y3wVvDbJHrU">"Watch my short video here, or read on."</a></b>
+            <br/>
+            { widget() }
+            <br/>
+            <br/>
+    <h1 id="about"><b>About</b></h1>
+    "I'm Tris, I'm a writer and producer of fast, technical videos, and audiofiction and music."
+    <br/>
+    "My first career was as a web developer, doing production on the side for 15 years, but in 2022 I accidentally become entirely self-employed thanks to the surprising success of my YouTube channel, No Boilerplate."
+    <br/>
+    <br/>
+    "
+    At heart I'm still a software developer, I'll re-use 100 libraries to avoid writing 10 lines of code - standing on the shoulders of giants is the only way I know how I get around."
+    <br/>
+    "But I've looked for a way to mark my videos and stories as being made by humans, not AI, and I can't find one that works in exactly the way I want."
+    <br/>
+    "I don't want something that says 'NO AI USED', signposts that are negative and judgemental, nor a '100% human made' guarantee - what would that even MEAN these days?"
         <br/>
-        { widget() }
+    "I want a positive mark."
         <br/>
         <br/>
-    })
+    "I have multiple issues with the options I've seen so far, from having multiple logos (which is confusing) to the fixation on AI being inherently evil (this will not always be the case)."
+    <br/>
+    "My root concern with these methods is that they are negative. `AI = bad`.
+    But I think the correct way to present this is `human = good`."
+
+    <br/>
+    <br/>
+
+    "I don't need 100% human made, I perhaps need 90% human made. Three example may make my thinking clearer:"
+    <ul>
+    <li>
+        "Using, say, chatgpt as a rhyming dictionary feels fine, but writing whole verses of your poem doesn't."
+    </li>
+    <li>
+        "Using DALL-E to start brainstorming with 100 generated views of birds sitting on telephone lines seems fine, but getting it to paint large sections of your artwork doesn't."
+    </li>
+    <li>
+        "Asking a text generator to give you 10 happy-sounding synonyms for despair sparks joy in me, but asking it to write your anti-trancendentalist masterpiece does not."
+    </li>
+    </ul>
+
+    <br/>
+
+    "Using these tools to make more of the artwork you want is valid, but you're not a creator, you're still a consumer. I'm not sure exactly what 'too much AI' is, but just like your audience, I'll know it when I see it."
+    <br/>
+    <br/>
+    "I love knowing a human made the artwork I'm consuming."
+    <br/>
+    "There's *something* there, something transcendent and magical."
+    <br/>
+    <br/>
+    "I *like* that you tried hard, that's part of the experience."
+    <br/>
+    <br/>
+    <br/>
+    "Humanmade is a NAMTAO production, made with <3 in 2024".
+        })
 }
 
 type Router<'a> = Vec<(&'a str, fn() -> Node)>;
