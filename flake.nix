6a4b2ad3bb3fbc43a4394b37b2ce52fa8f47e5b1
pwnwriter <hey@pwnwriter.xyz>
2024-07-22 16:27:09 +0545

    feat(flake): nix flake for dev env

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    in
+    {
+      devShells = builtins.listToAttrs (map
+        (system: {
+          name = system;
+          value =
+            let
+              pkgs = import nixpkgs { inherit system; };
+              buildInputs = with pkgs; [
+                pkg-config
+                openssl
+                libiconv
+              ] ++ (if builtins.elem system [ "x86_64-darwin" "aarch64-darwin" ] then [ pkgs.darwin.apple_sdk.frameworks.Security ] else []);
+            in
+            {
+              default = pkgs.mkShell {
+                buildInputs = buildInputs;
+
+                packages = with pkgs; [
+                   # cargo
+                   # rustc
+                   # rustfmt
+                ];
+
+              };
+            };
+        })
+        systems);
+    };
+}
